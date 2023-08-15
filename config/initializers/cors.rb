39c140af563101b331f19aedee3aa7c1243f54a8
takeo-t <takeo-t@118satellite.com>
2023-08-15 18:30:28 +0900

    cors設定の修正

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,8 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-        origins 'localhost:3001'
-        origins 'https://sta.orion-raptured.com/'
+        origins 'https://sta.orion-raptured.com'
 
   
         resource '*',
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,19 +4,19 @@
 # end
 
 
-# stations = JSON.parse(File.read(Rails.root.join('db', 'meitetu_sta.json')))
+stations = JSON.parse(File.read(Rails.root.join('db', 'meitetu_sta.json')))
 
-# stations.each do |station_data|
-#   Station2.create(station_data)
-# end
+stations.each do |station_data|
+  Station2.create(station_data)
+end
 
 # change_stations = JSON.parse(File.read(Rails.root.join('db','change_station.json')))
 # change_stations.each do |change_station_data|
 #   ChangeStation.create(change_station_data)
 # end
 
-change_stations = JSON.parse(File.read(Rails.root.join('db','change_station.json')))
-change_stations.each do |change_station_data|
-  change_station = ChangeStation.find_or_initialize_by(id: change_station_data["id"])
-  change_station.update(change_station_data)
-end
+# change_stations = JSON.parse(File.read(Rails.root.join('db','change_station.json')))
+# change_stations.each do |change_station_data|
+#   change_station = ChangeStation.find_or_initialize_by(id: change_station_data["id"])
+#   change_station.update(change_station_data)
+# end
